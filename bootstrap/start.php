b98dfc6588d6de98ec66afe292b2b2ef086d02cc
Dan Rovito <DanRovito@users.noreply.github.com>
2016-02-25 10:49:59 -0500

    Create start.php

---
+++ bootstrap/start.php
@@ -0,0 +1,40 @@
+<?php
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+
+session_start();
+
+require '../vendor/autoload.php';
+require '../config/database.php';
+
+//Require dotenv notation
+// Environment based configuration
+// Allows developers using the getenv('<name>') method to fetch configuration values
+if (file_exists('../.env')) {
+    $dotenv = new Dotenv\Dotenv('../');
+    $dotenv->load();
+}
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => getenv('DEBUG')
+    ]
+]);
+
+$app->db = function(){
+	return new Capsule;
+};
+
+$container = $app->getContainer();
+//Setup View handler
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('../app/views');
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+
+    return $view;
+};
+
+require 'routes.php';
