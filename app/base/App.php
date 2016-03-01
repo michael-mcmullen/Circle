efdd745992a7e47ca962c148e85efaf0d6f886bd
Dan Rovito <danrovito@gmail.com>
2016-03-01 08:38:11 -0500

    routing start

--- app/base/App.php
+++ app/base/App.php
@@ -2,7 +2,20 @@
 
 class App
 {
+    protected $controller = 'home';
+    protected $method     = 'index';
+    protected $params     = [];
+
     public function __construct()
     {
+       return print_r($this->parseUrl());
+    }
+
+    public function parseUrl()
+    {
+        if(isset($_GET['url']))
+        {
+            return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
     }
 }
