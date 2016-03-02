282197769203f6f0e4e0e80efe2dee9b32de1847
Dan Rovito <danrovito@gmail.com>
2016-03-01 22:55:28 -0500

    views, controllers, and models

--- app/base/App.php
+++ app/base/App.php
@@ -8,7 +8,39 @@ class App
 
     public function __construct()
     {
-       return print_r($this->parseUrl());
+        $url = $this->parseUrl();
+
+        /*
+            This uses the controller in the url.
+         */
+        if(file_exists('../app/http/controllers/' . $url[0] . '.php'))
+        {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/http/controllers/' . $this->controller . '.php';
+
+        $this->controller = new $this->controller;
+        /*
+            This uses the method passed in the url.
+         */
+        if(isset($url[1]))
+        {
+            if(method_exists($this->controller, $url[1]))
+            {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+        /*
+            This uses the parameters sent after the controller and method.
+         */
+        $this->params = $url ? array_values($url) : [];
+        /*
+            Calls the method and params sent to the controller.
+         */
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseUrl()
