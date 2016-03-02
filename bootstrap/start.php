aabefb0cfccd734a4cfd7006e659ebf73eec9971
Dan Rovito <DanRovito@users.noreply.github.com>
2016-03-02 14:23:18 -0500

    Update start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -3,3 +3,11 @@
 require '../vendor/autoload.php';
 require_once '../app/base/App.php';
 require_once '../app/base/Controller.php';
+
+//Require dotenv notation
+// Environment based configuration
+// Allows developers using the getenv('<name>') method to fetch configuration values
+if (file_exists('../.env')) {
+    $dotenv = new Dotenv\Dotenv('../');
+    $dotenv->load();
+}
