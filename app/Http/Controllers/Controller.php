5eeb970219bae2b47bf490489570665f32c9f63e
Dan Rovito <danrovito@gmail.com>
2016-04-01 16:19:05 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,10 +6,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Controller
 {
-  public function view($view, $data = [])
-  {
-    require_once '../app/resources/views/' . $view . '.php';
-    
-    return new Response();
-  }
+    public function view($view, $data = [])
+    {
+        require_once '../app/resources/views/'.$view.'.php';
+
+        return new Response();
+    }
 }
