fe3728235d3b0fb936df17d76f9b57b51635555d
Dan Rovito <danrovito@gmail.com>
2016-04-01 23:10:33 -0400

    removed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,12 @@ namespace Http\Controllers;
 
 use Symfony\Component\HttpFoundation\Response;
 
-class Controller extends BaseController
+class Controller
 {
+    public function view($view, $data = [])
+    {
+        require_once '../app/resources/views/'.$view.'.php';
 
+        return new Response();
+    }
 }
