4dc112fc825edc3345eee2e0a9dbf12d2f4018ea
Dan Rovito <danrovito@gmail.com>
2016-04-01 22:04:55 -0400

    fixing home view

--- app/resources/views/home.php
+++ app/resources/views/home.php
@@ -1,1 +1,51 @@
-Circle Framework
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Circle Framework</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+
+        <style>
+            html, body {
+                font-family: 'Open Sans', sans-serif;
+                height: 100%;
+            }
+
+            body {
+                width: 100%;
+                display: table;
+                margin: 0;
+                padding: 0;                
+            }
+
+            .container {
+                vertical-align: middle;
+                display: table-cell;
+                text-align: center;
+            }
+            .home-heading {
+                font-size: 46px;
+                font-weight: 400;
+                line-height: 56px;
+                margin-bottom: 15px;
+                color: #666;
+            }
+            .home-text {
+                font-size: 22px;
+                font-weight: 300;
+                line-height: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+                <div class="home-heading">
+                Circle Framework
+            </div>
+            <div class="home-text">
+                A PHP MVC Framework
+            </div>
+        </div>
+    </body>
+</html>
