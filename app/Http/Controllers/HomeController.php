d574b57044652492aa1b4fd156d43cca26024eaa
Dan Rovito <DanRovito@users.noreply.github.com>
2016-05-06 15:07:54 -0400

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
+    
+    public function __construct(
+        //$baseUrl = '',
+        $method = 'GET',
+        //$host = 'localhost',
+        //$scheme = 'http',
+        //$httpPort = 80,
+        //$httpsPort = 443,
+        //$path = '/',
+        //$queryString = ''
+    )
+    
     public function index()
     {
         return $this->view('home');
